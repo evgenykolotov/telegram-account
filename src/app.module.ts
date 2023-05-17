397884df02d67ab38da2ae141dcae4598833ea86
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2023-05-17 23:13:18 +0300

    feat: Установлен и настроен npm-пакет @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `environment/.${process.env.NODE_ENV}.env`,
+		}),
+	],
 })
 export class AppModule {}
